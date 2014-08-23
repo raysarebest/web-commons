9af914f22bb51176862a453d0c3be6b0f28d15d5
Michael Hulet <bestraysfan@me.com>
2014-08-23 01:12:06 -0400

    Totally automated notifications.js by taking event handling code functions as parameters to the notify() function

--- js/notifications.js
+++ js/notifications.js
@@ -1,5 +1,5 @@
 //This file is a template to use Local Notifications, as defined by the W3C to the fullest of its ability. It is wrapped inside of a function (notify(heading, message, uniqueTag)), so it can be called later
-function notify(title, body, tag){
+function notify(title, body, tag, onshow, onclick, onclose, onerror){
   //First, we have to make sure that the title is a string
   if(typeof title != "string"){
     //If it's not, we try to convert it to a string and continue
@@ -53,22 +53,53 @@ function notify(title, body, tag){
     else if(Notification.permission === 'granted'){
       //This creates the actual notification, sets all of its values, and displays it
       var n = new Notification(title,{"body":body,"tag":tag});
+      //This event is fired as soon as the notification appears onscreen
       n.onshow = function(){
-        //You can execute code here as soon as the notification is shown onscreen if you want, but we won't do anything right now
-        console.log("onshow ran");
+        //If any code was sent for this event, we run it
+        try{
+          onshow();
+        }
+        //If not, we just tell that to the console
+        catch(e){
+          console.log("No valid function specified for the onshow event");
+        }
       };
+      //This event is fired when the user clicks on the notification
       n.onclick = function(){
-        //You can execute whatever you want when the user clicks the notification here, but for now, we'll just dismiss the Notification
+        //First, we automatically close the notification
         this.close();
-        console.log("onclick ran");
+        //Then, we run any code specified by the developer, if there is any
+        try{
+          onclick();
+        }
+        //If there isn't, we log the instance to the console
+        catch(e){
+          console.log("No valid function was specified for the onclick event");
+        }
       };
+      //This event is fired as soon as the notification is closed
       n.onclose = function(){
-        //You can execute whatever you want when the notification is closed here, but we won't do anything right now
-        console.log("onclose ran");
+        //We try to run any code passed to us by the developer, if it exists
+        try{
+          onclose();
+        }
+        //If there isn't any, we log this fact to the console
+        catch(e){
+          console.log("No valid function was specified for the onclose event");
+        }
       };
+      //This event fires when there's an error displaying the notification, usually because of a default or denied Notification.permission status. We handled these errors above, so this should never run, but if it does, this is here, just in case
       n.onerror = function(){
-        //This usually shouldn't execute (it usually only would if Notification.permission is either default or denied), but if it does, you can do whatever you want here, but we'll just log to the console that it did run right now
+        //First, we log to the console that an error occurred
         console.log("There was an error displaying the notification");
+        //Then we try to run any code passed to us for this scenario by the developer
+        try{
+          onerror();
+        }
+        //If there isn't any, we log that to the console
+        catch(e){
+          console.log("No valid function was specified for the onerror event");
+        }
       };
     }
   }
