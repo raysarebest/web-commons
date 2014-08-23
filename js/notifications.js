e6574cda1ce1a7b0fd03320c2603a85f754b9fa0
Michael Hulet <bestraysfan@me.com>
2014-08-23 00:31:56 -0400

    Significantly cleaned up parameter checking/handling code

--- js/notifications.js
+++ js/notifications.js
@@ -1,47 +1,40 @@
 //This file is a template to use Local Notifications, as defined by the W3C to the fullest of its ability. It is wrapped inside of a function (notify(heading, message, uniqueTag)), so it can be called later
 function notify(title, body, tag){
-  //If either title or body wasn't given, or has a data type unusable in this situation, we'll log it to the console and return, so the rest of the function doesn't run
-  if(typeof title === "undefined" || typeof title === "object" || typeof title === "function" || typeof body === "undefined" || typeof body === "object" || typeof body === "function"){
-    console.log("The notification title and/or body wasn't given, or was not a string or value that could be converted to a string");
+  //First, we have to make sure that the title is a string
+  if(typeof title != "string"){
+    //If it's not, we try to convert it to a string and continue
+    try{
+      title.toString();
+    }
+    //If that can't be done, we log the instance to the console and stop executing
+    catch(e){
+      console.log("The notification title wasn't given, or was not a string or value that could be converted to a string");
     return;
-  }
-  //If title or body is something that can be converted to a string, we do so and continue
-  else if(typeof title === "number" || typeof title === "boolean" || typeof body === "number" || typeof body === "boolean"){
-    //First, we need to check to see if it's the title that's causing issues
-    if(typeof title != "string"){
-      //If it is, we convert it
-      title = title.toString();
-    }
-    //Otherwise, we know it's body that's causing the issue, so we convert that
-    else{
-      body = body.toString();
     }
   }
-  //Array doesn't have a typeof for some reason (?), so we need to conver it if body or title is that
-  else if(typeof title != "string" || typeof body != "string"){
-    //Again, we need to see if title is causing the problem
-    if(typeof title != "string"){
-      //Again, we convert switch
-      title = title.toString();
+  //We also have to make sure that body is a string
+  if(typeof body != "string"){
+    //If it's not, we try to convert it to a string
+    try{
+      body.toString();
     }
-    //Otherwise, we know that body is the problem, and therefore convert
-    else{
-      body = body.toString();
+    //If that can't be done, we log the instance to the console and stop executing
+    catch(e){
+      console.log("The notification title body wasn't given, or was not a string or value that could be converted to a string");
+    return;
     }
   }
-  //So the uniqueTag paramater can be optional, if it's undefined or something else that's not useful in this situation, we set it to a random number between 0 and 999,999,999,999, which is probably never reproducible (we'll return it later, just in case, though), but it's worth a shot. It'll at least guarantee that they'll never be dismissed until the user dismisses it
-  if(typeof tag === "undefined" || typeof tag === "object" || typeof tag === "function"){
-    tag = Math.round(Math.random()*999999999999).toString();
-    //If there wasn't a tag given, or it wasn't a useable data type for this purpose, we can loge that to the console
-    console.log("No unique tag given, or was not a data type compatibe for assignment");
-  }
-  //If it's something that has a .toString() method, we can help the developer out and turn their bad type into a good one
-  else if(typeof tag === "number" || typeof tag === "boolean"){
-    tag = tag.toString();
-  }
-  //For some reason, typeof "array" doesn't exist (?), so we can know it's an array here, and convert it to a string
-  else if(typeof tag != "string"){
-    tag = tag.toString();
+  //Finally, we make sure that the tag is a string
+  if(typeof tag != "string"){
+    //If it's not, we try to convert it to one
+    try{
+      tag.toString();
+    }
+    //If that can't be done, we generate a random number between 0 and 999,999,999,999 and convert that to a string, and assign it to tag (so the parameter can be optional). This method probably won't produce identical tags, ever, so it probably won't stop duplicate notifications, but we return the tag later, in the hopes that the programmer might use it for their own duplicates prevention. Finally, we log this instance to the console
+    catch(e){
+      tag = Math.round(Math.random()*999999999999).toString();
+      console.log("No unique tag given, or was not a data type compatibe for assignment");
+    }
   }
   //Now the magic can actually begin.. First, we check if the host actually supports notifications
   if(window.Notification){
